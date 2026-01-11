9a78e968da57cc71100717eb1714497403bccb7d
HTStrix7Coder <161967856+HTStrix7Coder@users.noreply.github.com>
2026-01-11 21:33:53 +0530

    Clean up smoothie order logic by removing unused code
    Removed unnecessary requests and dataframe display for watermelon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -39,10 +41,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
